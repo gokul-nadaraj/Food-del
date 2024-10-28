2537451de61666a646bd7d09178f266106cbcb3e
gokulnadaraj@gmail.com <167672053+gokul-nadaraj@users.noreply.github.com>
2024-10-28 21:12:23 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home/Home'
 import Placeorder from './pages/placeorder/Placeorder'
 import Cart from './pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
-import MyOrder from './pages/myorders/MyOrder';
+import MyOrder from './pages/myorders/MyOrder.jsx'
 import Loginpopup from './components/Loginpopup/Loginpopup'
 import { Verify } from './pages/verify/Verify'
 
